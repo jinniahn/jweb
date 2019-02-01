1d14e6d7ba0a8f68a71ff9ce878686d47c8b1db9
jinsub ahn <jinniahn@gmail.com>
2019-02-01 16:32:45 +0900

    enable to add name and option for new browser

--- jweb/web_client.py
+++ jweb/web_client.py
@@ -16,13 +16,13 @@ curdir = os.path.abspath(os.path.dirname(__file__))
 class WebClient(object):
     ''' web client '''
     
-    def __init__(self):
-        self.conn = rpyc.classic.connect('localhost')
+    def __init__(self, host='localhost', port=18812, option=None, name=None):
+        self.conn = rpyc.classic.connect(host, port)
         self.selenium = self.conn.modules['selenium']
         self.ui = self.conn.modules['selenium.webdriver.support.ui']
-        self.driver = self._getWebDriver()        
+        self.driver = self._getWebDriver(name, option)
 
-    def _createDriver(self):
+    def _createDriver(self, name = None, option=None):
         'create selenium dirver'
 
         lines = '''\
@@ -30,23 +30,42 @@ class WebClient(object):
         from selenium import webdriver
         selenium.cur_driver = webdriver.Chrome()
         '''
+        
+        if option:
+            lines = option
+
+        lines += '''\
+        if not hasattr(selenium, 'cur_drivers'): selenium.cur_drivers = {}
+        '''
+
         for l in lines.splitlines():
             l = l.strip()
             self.conn.execute(l)
 
         driver = self.conn.modules.selenium.cur_driver
+
+            
+        if name:
+            drivers = self.conn.modules.selenium.cur_drivers
+            print(drivers)
+            drivers[name] = driver
+        
         return driver
 
-    def _getWebDriver(self):
+    def _getWebDriver(self, name=None, option=None):
         try:
-            driver = self.conn.modules.selenium.cur_driver
+            if name:
+                print(self.conn.modules.selenium.cur_drivers)
+                driver = self.conn.modules.selenium.cur_drivers[name]
+            else:
+                driver = self.conn.modules.selenium.cur_driver
         except Exception as e:
-            return self._createDriver()
+            return self._createDriver(name, option)
 
         try:
             driver.current_url
         except Exception as e:
-            return self._createDriver()
+            return self._createDriver(name, option)
         return driver
 
     def run_js(self, code, jquery=True):
