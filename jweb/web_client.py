9a6dd3690f0edbf464a4eb79fcfbd2ef97cf395b
jinsub ahn <jinniahn@gmail.com>
2019-02-28 09:18:53 +0900

    support direct-connection to selenium

--- jweb/web_client.py
+++ jweb/web_client.py
@@ -11,7 +11,78 @@ import rpyc
 
 from pprint import pprint
 
+
 curdir = os.path.abspath(os.path.dirname(__file__))
+
+class EmbedWebClient(object):
+    ''' web client '''
+    
+    def __init__(self, name=None, option=None):
+        import selenium
+        self.selenium = selenium
+        self.driver = self._getWebDriver(name, option)
+
+    def _createDriver(self, name = None, option=None):
+        'create selenium dirver'
+
+        lines = '''\
+        import selenium
+        from selenium import webdriver
+        selenium.cur_driver = webdriver.Chrome()
+        '''
+        
+        if option:
+            lines = option
+
+        lines += '''\
+        if not hasattr(selenium, 'cur_drivers'): selenium.cur_drivers = {}
+        '''
+
+        for l in lines.splitlines():
+            l = l.strip()
+            exec(l)
+
+        driver = self.selenium.cur_driver
+
+            
+        if name:
+            drivers = self.selenium.cur_drivers
+            drivers[name] = driver
+        
+        return driver
+
+    def _getWebDriver(self, name=None, option=None):
+        try:
+            if name:
+                driver = self.selenium.cur_drivers[name]
+            else:
+                driver = self.selenium.cur_driver
+        except Exception as e:
+            return self._createDriver(name, option)
+
+        try:
+            driver.current_url
+        except Exception as e:
+            return self._createDriver(name, option)
+        return driver
+
+    def run_js(self, code, jquery=True):
+        '''run javascript code'''
+
+        if jquery:
+            jscode = 'return window["jQuery"] != undefined'
+            is_existed = self.driver.execute_script(jscode)
+        
+            if not is_existed:
+                jscode = open(os.path.join(curdir,'data/jquery.js')).read()
+                self.driver.execute_script(jscode)
+
+        return self.driver.execute_script(code)
+
+    def get(self, url):
+        self.driver.get(url)
+
+
 
 class WebClient(object):
     ''' web client '''
@@ -47,7 +118,6 @@ class WebClient(object):
             
         if name:
             drivers = self.conn.modules.selenium.cur_drivers
-            print(drivers)
             drivers[name] = driver
         
         return driver
@@ -55,7 +125,6 @@ class WebClient(object):
     def _getWebDriver(self, name=None, option=None):
         try:
             if name:
-                print(self.conn.modules.selenium.cur_drivers)
                 driver = self.conn.modules.selenium.cur_drivers[name]
             else:
                 driver = self.conn.modules.selenium.cur_driver
@@ -83,3 +152,7 @@ class WebClient(object):
 
     def get(self, url):
         self.driver.get(url)
+
+
+
+
