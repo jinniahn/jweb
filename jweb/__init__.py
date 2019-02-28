fa6e55204df10098c0a14f8cc7ca028e708e54da
jinsub ahn <jinniahn@gmail.com>
2019-02-28 09:19:31 +0900

    version: 0.1.1

--- jweb/__init__.py
+++ jweb/__init__.py
@@ -1,5 +1,5 @@
 """jweb - An opinionated, minimal cookiecutter template for Python packages"""
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'jinsub ahn <jinniahn@gmail.com>'
 __all__ = []
